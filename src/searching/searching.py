89d68dcd81fbf65d5c7b31f3135f7bce4ed9ea76
Devanee <devaneereid@gmail.com>
2020-07-20 17:06:54 -0600

    finished binary_search, tests passing

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -11,5 +11,20 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    first = 0
+    last = (len(arr) -1)
+ 
+    while first <= last:
+        middle = (first + last)
+
+        if arr[middle] == target:
+            return middle
+
+        else: 
+            if target < arr[middle]:
+                last = middle -1
+
+            else: 
+                first = middle + 1
 
     return -1  # not found
